2c19a85a0081a17bacc37adb47966000182474bd
Danylo Oliinyk <danylo.oliinyk@gmail.com>
2024-07-16 00:25:10 +0300

    Green Button works

--- app/src/main/java/com/gmail/danylo/oliinyk/composetest/GreenButton.kt
+++ app/src/main/java/com/gmail/danylo/oliinyk/composetest/GreenButton.kt
@@ -1,9 +1,10 @@
 package com.gmail.danylo.oliinyk.composetest
 
+import android.graphics.BlurMaskFilter
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -21,34 +22,27 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.BlurredEdgeTreatment
-import androidx.compose.ui.draw.blur
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.ColorMatrix
 import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.Shadow
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.gmail.danylo.oliinyk.composetest.Colors.darkerGreen
 import com.gmail.danylo.oliinyk.composetest.Colors.lighterGreen
 import com.gmail.danylo.oliinyk.composetest.Fonts.PT_SANS
-import kotlin.math.ceil
 
 private val BUTTON_HEIGHT_DP = 234.dp
 private val BUTTON_BOTTOM_PADDING_DP = 16.dp
@@ -74,8 +68,6 @@ fun GreenButton() {
             animationSpec = tween(durationMillis = if (isPressed) 75 else 150),
             label = "Color1Animation"
         )
-        val notPressedWhiteOverlayBrush = createBrush(0.21f, 0f, 0.43f)
-        val pressedWhiteOverlayBrush = createBrush(0.07f, 0f, 0.15f)
         val whiteOverlayBrush = createBrush(alpha1, alpha2, alpha3)
         val innerBorderWhiteOverlayBrush = createBrush(0.34f, 1f, 0.7f)
 
@@ -94,139 +86,106 @@ fun GreenButton() {
             animationSpec = tween(durationMillis = if (isPressed) 75 else 150),
             label = "offsetYAnimation"
         )
-//        Shadowed(
-//            modifier = Modifier.align(Alignment.Center),
-//            color = Color(0x39000029),
-//            offsetX = 0.dp,
-//            offsetY = 3.dp,
-//            blurRadius = 3.dp
-//        ) {
-            Box(
+        val shadowColor by animateColorAsState(
+            targetValue = if (isPressed) Color.Transparent else Color(0x35000029),
+            animationSpec = tween(durationMillis = if (isPressed) 75 else 150),
+            label = "shadowColorAnimation"
+        )
+        Box(
+            modifier = Modifier
+                .width(222.dp)
+                .height(buttonHeight)
+                .shadowCustom(
+                    color = shadowColor,
+                    offsetY = 3.dp,
+                    shapeRadius = 19.dp,
+                    blurRadius = 3.dp,
+                )
+                .offset(y = offsetY)
+                .clip(RoundedCornerShape(19.dp))
+                .background(color = lighterGreen)
+                .padding(start = 1.dp, top = 1.dp, end = 1.dp, bottom = bottomPadding)
+                .clip(RoundedCornerShape(19.dp))
+                .background(brush = innerBorderWhiteOverlayBrush)
+                .padding(top = 1.dp, bottom = 1.dp)
+                .clip(RoundedCornerShape(18.dp))
+                .background(color = lighterGreen)
+                .background(brush = whiteOverlayBrush)
+                .align(Alignment.Center)
+                .clickable(
+                    interactionSource = interactionSource,
+                    indication = null,
+                    onClick = { /* Important action */ }
+                )
+        ) {
+            Text(
                 modifier = Modifier
-                    .width(222.dp)
-                    .height(buttonHeight)
-                    .offset(y = offsetY)
-                    .clip(RoundedCornerShape(19.dp))
-                    .background(color = lighterGreen)
-                    .padding(start = 1.dp, top = 1.dp, end = 1.dp, bottom = bottomPadding)
-                    .clip(RoundedCornerShape(19.dp))
-                    .background(brush = innerBorderWhiteOverlayBrush)
-                    .padding(top = 1.dp, bottom = 1.dp)
-                    .clip(RoundedCornerShape(18.dp))
-                    .background(color = lighterGreen)
-                    .background(brush = whiteOverlayBrush)
-                    .align(Alignment.Center)
-                    .clickable(
-                        interactionSource = interactionSource,
-                        indication = null,
-                        onClick = { /* Important action */ }
-                    )
-            ) {
-                Text(
-                    modifier = Modifier
-                        .align(Alignment.Center),
-                    text = stringResource(id = R.string.start),
-                    fontSize = 38.sp,
-                    fontFamily = PT_SANS,
-                    color = Color.White,
-                    fontWeight = FontWeight(400),
-                    style = TextStyle.Default.copy(
-                        shadow = Shadow(
-                            color = darkerGreen,
-                            offset = Offset(0f, 1f),
-                            blurRadius = 2f
-                        ),
-                    )
+                    .align(Alignment.Center),
+                text = stringResource(id = R.string.start),
+                fontSize = 38.sp,
+                fontFamily = PT_SANS,
+                color = Color.White,
+                fontWeight = FontWeight(400),
+                style = TextStyle.Default.copy(
+                    shadow = Shadow(
+                        color = darkerGreen,
+                        offset = Offset(0f, 1f),
+                        blurRadius = 2f
+                    ),
+                )
+            )
+        }
+    }
+}
+
+fun Modifier.shadowCustom(
+    color: Color = Color.Black,
+    offsetX: Dp = 0.dp,
+    offsetY: Dp = 0.dp,
+    blurRadius: Dp = 0.dp,
+    shapeRadius: Dp = 0.dp,
+) = composed {
+    val paint: Paint = remember { Paint() }
+    val blurRadiusPx = blurRadius.px(LocalDensity.current)
+    val maskFilter = remember {
+        BlurMaskFilter(blurRadiusPx, BlurMaskFilter.Blur.NORMAL)
+    }
+    drawBehind {
+        drawIntoCanvas { canvas ->
+            val frameworkPaint = paint.asFrameworkPaint()
+            if (blurRadius != 0.dp) {
+                frameworkPaint.maskFilter = maskFilter
+            }
+            frameworkPaint.color = color.toArgb()
+
+            val leftPixel = offsetX.toPx()
+            val topPixel = offsetY.toPx()
+            val rightPixel = size.width + leftPixel
+            val bottomPixel = size.height + topPixel
+
+            if (shapeRadius > 0.dp) {
+                val radiusPx = shapeRadius.toPx()
+                canvas.drawRoundRect(
+                    left = leftPixel,
+                    top = topPixel,
+                    right = rightPixel,
+                    bottom = bottomPixel,
+                    radiusX = radiusPx,
+                    radiusY = radiusPx,
+                    paint = paint,
+                )
+            } else {
+                canvas.drawRect(
+                    left = leftPixel,
+                    top = topPixel,
+                    right = rightPixel,
+                    bottom = bottomPixel,
+                    paint = paint,
                 )
             }
-//        }
-    }
-}
-
-private fun Modifier.surface(
-    shape: Shape,
-    backgroundColor: Color,
-    border: BorderStroke?,
-    elevation: Dp
-) = this
-    .shadow(elevation, shape, clip = false)
-    .background(color = backgroundColor, shape = shape)
-    .clip(shape)
-
-@Composable
-fun Shadowed(
-    modifier: Modifier = Modifier,
-    color: Color,
-    offsetX: Dp,
-    offsetY: Dp,
-    blurRadius: Dp,
-    content: @Composable () -> Unit
-) {
-    val density = LocalDensity.current
-    val offsetXPx = with(density) { offsetX.toPx() }.toInt()
-    val offsetYPx = with(density) { offsetY.toPx() }.toInt()
-    val blurRadiusPx = ceil(with(density) {
-        blurRadius.toPx()
-    }).toInt()
-
-    // Modifier to render the content in the shadow color, then
-    // blur it by blurRadius
-    val shadowModifier = Modifier
-        .drawWithContent {
-            val matrix = shadowColorMatrix(color)
-            val filter = ColorFilter.colorMatrix(matrix)
-            val paint = Paint().apply {
-                colorFilter = filter
-            }
-            drawIntoCanvas { canvas ->
-                canvas.saveLayer(Rect(0f, 0f, size.width, size.height), paint)
-                drawContent()
-                canvas.restore()
-            }
-        }
-        .blur(radius = blurRadius, BlurredEdgeTreatment.Unbounded)
-        .padding(all = blurRadius) // Pad to prevent clipping blur
-
-    // Layout based solely on the content, placing shadow behind it
-    Layout(modifier = modifier, content = {
-        // measurables[0] = content, measurables[1] = shadow
-        content()
-        Box(modifier = shadowModifier) { content() }
-    }) { measurables, constraints ->
-        // Allow shadow to go beyond bounds without affecting layout
-        val contentPlaceable = measurables[0].measure(constraints)
-        val shadowPlaceable = measurables[1].measure(Constraints(maxWidth = contentPlaceable.width + blurRadiusPx * 2,
-            maxHeight = contentPlaceable.height + blurRadiusPx * 2))
-        layout(width = contentPlaceable.width, height = contentPlaceable.height) {
-            shadowPlaceable.placeRelative(x = offsetXPx - blurRadiusPx, y = offsetYPx - blurRadiusPx)
-            contentPlaceable.placeRelative(x = 0, y = 0)
         }
     }
 }
 
-// Return a color matrix with which to paint our content
-// as a shadow of the given color
-private fun shadowColorMatrix(color: Color): ColorMatrix {
-    return ColorMatrix().apply {
-        set(0, 0, 0f) // Do not preserve original R
-        set(1, 1, 0f) // Do not preserve original G
-        set(2, 2, 0f) // Do not preserve original B
-
-        set(0, 4, color.red * 255) // Use given color's R
-        set(1, 4, color.green * 255) // Use given color's G
-        set(2, 4, color.blue * 255) // Use given color's B
-        set(3, 3, color.alpha) // Multiply by given color's alpha
-    }
-}
-
-//fun Modifier.moveOnPress(
-//    byDp: Dp,
-//    interactionSource: InteractionSource
-//) = composed {
-//    val isPressed by interactionSource.collectIsPressedAsState()
-//    val animatedHeight = animateDpAsState(
-//        targetValue = byDp,
-//        animationSpec = tween(durationMillis = 75),
-//        label = "moveOnPress"
-//    )
-//}
+private fun Dp.px(density: Density): Float =
+    with(density) { toPx() }
